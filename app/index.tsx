aac865db9b56cd14311785e8224071120e2b49d4
fredo09 <fred_15@outlook.com>
2025-02-07 00:08:23 -0600

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const App = () => {
+    return (
+        <View>
+            <Text>Hola mundo!! XD 🚀 </Text>
+        </View>
+    )
+}
+
+export default App;
