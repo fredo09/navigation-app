c62c2e69b8196b44b7ed56747a155079654c92e6
fredo09 <fred_15@outlook.com>
2025-02-11 01:18:26 -0600

    add fonts and config

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,27 @@
-import React from 'react'
-import { Slot } from 'expo-router';
+import React, { useEffect } from 'react'
+import { Slot, SplashScreen } from 'expo-router';
+import { useFonts } from 'expo-font';
 
 import './global.css';
 
-const RootLayout = () => <Slot />;
+//! Evita que el SplashScreen se quite hasta que se cargen las funetes
+SplashScreen.preventAutoHideAsync();
+
+const RootLayout = () =>  {
+    const [ fontsLoaded, error ] = useFonts({
+        'WorkSans-Black': require('../assets/fonts/WorkSans-Black.ttf'),
+        'WorkSans-Light': require('../assets/fonts/WorkSans-Light.ttf'),
+        'WorkSans-Medium': require('../assets/fonts/WorkSans-Medium.ttf'),
+    });
+
+    useEffect(() => {
+        if (error) throw error;
+        if (fontsLoaded) SplashScreen.hideAsync(); //! -> Ocultamos el SplashScreen
+    }, [fontsLoaded, error]);
+
+    if (!fontsLoaded && !error ) return null;
+
+    return <Slot/>;
+};
 
 export default RootLayout;
--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, SafeAreaView } from 'react-native';
 
 const App = () => {
     return (
-        <View className='mt-10'>
-            <Text className='text-3xl font-bold text-blue-500'>
-                Hola mundo!! XD 🚀
-            </Text>
-        </View>
+        <SafeAreaView>
+            <View className='mt-6 mx-2.5'>
+                {/* Forma de usar y ligar los fonts */}
+                <Text className='text-3xl' style={{ fontFamily: 'WorkSans-Black' }}>
+                    Hola mundo!! XD 🚀
+                </Text>
+                {/* Forma de usar y ligar de font s */}
+                <Text className='text-2xl font-work-medium'>
+                    Hola mundo!! XD 🚀
+                </Text>
+                <Text className='text-1xl font-work-light'>
+                    Hola mundo!! XD 🚀
+                </Text>
+            </View>
+        </SafeAreaView>
     )
 }
 
